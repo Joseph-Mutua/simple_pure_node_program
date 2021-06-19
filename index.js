5e03fbac1b1ca8524b4bed0458dadb116496c948
Joseph Mutua <mutuaj793@gmail.com>
2021-06-19 23:02:40 +0300

    Add routing for homepage and about page

--- index.js
+++ index.js
@@ -6,10 +6,27 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
-    //Set Contet-type
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("<h1>Home</h1>");
+    //Set Content-type
+
+    fs.readFile(path.join(__dirname, "public", "index.html"), (err, data) => {
+      if (err) throw err;
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    });
   }
+  if (req.url === "/about") {
+    //Set Content-type
+
+    fs.readFile(path.join(__dirname, "public", "about.html"), (err, data) => {
+      if (err) throw err;
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    });
+  }
+
+
+
+  
 });
 
 server.listen(port, () => {
